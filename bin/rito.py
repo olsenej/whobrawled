940fff861d39eb7481e3f6eb0e4b8c9b0a034ef3
Edward Olsen <olsenedw@msu.edu>
2015-08-18 05:39:12 +0000

    Server setup

---
+++ bin/rito.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from random import randint
+from contextlib import closing
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask_bootstrap import Bootstrap
+import pymysql
+import re
+
+# create our little application :)
+app = Flask(__name__)
+Bootstrap(app)
+app.config.from_object(__name__)
+app.debug=True
+
+
+def connect_db():
+	return pymysql.connect(host='localhost', user='ec2-user', passwd='pdcd', db='rito')
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
+@app.route('/')
+def index():
+
+	return render_template('index.html')
+
+
+@app.route('/images')
+def render_image(image_path):
+	image_path='/images/'
+	return send_from_directory('',image_path)  
+
+
+if __name__ == '__main__':
+    app.run()
+
